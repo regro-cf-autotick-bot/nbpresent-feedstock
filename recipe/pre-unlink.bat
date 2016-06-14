20e7d60836af710a751395c8f80b368f5c59a976
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-13 22:50:15 -0400

    use block-style output redirect

--- recipe/pre-unlink.bat
+++ recipe/pre-unlink.bat
@@ -1,2 +1,5 @@
-"%PREFIX%\Scripts\jupyter-nbextension" disable nbpresent --py --sys-prefix > NUL 2>&1
-"%PREFIX%\Scripts\jupyter-serverextension" disable nbpresent --py --sys-prefix > NUL 2>&1
+@echo off
+1>>"%PREFIX%\.messages.txt" 2>&1 (
+  "%PREFIX%\Scripts\jupyter-nbextension" disable nbpresent --py --sys-prefix
+  "%PREFIX%\Scripts\jupyter-serverextension" disable nbpresent --py --sys-prefix
+)
